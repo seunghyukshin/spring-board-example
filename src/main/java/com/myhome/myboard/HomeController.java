e482fa130c1c6a3aabd9f2f85b7beaed9183c0f4
seunghyukshin <auseunghyuk@gmail.com>
2022-03-23 23:43:41 +0900

    studied ModelAndView

--- src/main/java/com/myhome/myboard/HomeController.java
+++ src/main/java/com/myhome/myboard/HomeController.java
@@ -2,12 +2,27 @@ package com.myhome.myboard;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
 	@GetMapping("/")
 	public String index() {
+		// view만 리턴
 		return "index";
+	}
+
+	@RequestMapping("/hello.do")
+	public ModelAndView hello(String name) {
+		// 데이터도 같이 return
+
+		ModelAndView mav = new ModelAndView();
+
+		// request.setAttribute();
+		mav.addObject("msg", "Hello " + name);
+		mav.setViewName("hello/hellomsg"); // WEB-INF/views/hellomsg.jsp
+		return mav;
 	}
 }
--- src/main/webapp/WEB-INF/views/hello/hellomsg.jsp
+++ src/main/webapp/WEB-INF/views/hello/hellomsg.jsp
@@ -1,13 +1,10 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <%@ page session="false"%>
-<c:set var="root" value="${pageContext.request.contextPath}" />
 <html>
 <head>
 <title>MyBoard</title>
 </head>
 <body>
-	<div align="center">
-		<a href="${root}/hello.do?name=Spring"> Main </a> <br>
-	</div>
+	<div align="center">${msg}</div>
 </body>
 </html>
